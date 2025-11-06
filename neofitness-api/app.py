fc6dfd6a5cce193470488047c9d809ca9cf5b595
Nguyen Quoc Khanh <khenng208@gmail.com>
2025-11-07 02:44:45 +0700

    xoa file cu

--- neofitness-api/app.py
+++
@@ -1,8 +0,0 @@
-from flask_cors import CORS
-from app import create_app
-
-app = create_app()
-CORS(app, resources={r"/api/*": {"origins": "http://localhost:5173"}})  # Cho phép origin của frontend
-
-if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug=True)
